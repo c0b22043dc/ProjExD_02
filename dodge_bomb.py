420f1b030f4031ef368194e2d8fbec5167c0ea06
riku.k <c0b22043@edu.teu.ac.jp>
2023-11-28 16:55:49 +0900

    追加機能1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,6 +10,7 @@ delta = {
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0)
 }
+
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:  #練習４　爆弾判定
     yoko, tate = True, True
     if rct.left < 0 or WIDTH < rct.right:
@@ -21,12 +22,20 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:  #練習４　爆弾判定
 
 
 
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    #追加機能1　キーによって方向変換
+    kk_img_up= pg.transform.rotozoom(kk_img, -90, 1.0)
+    kk_img_down = pg.transform.rotozoom(kk_img, 90, 1.0)
+    kk_img_left = pg.transform.rotozoom(kk_img, 0, 1.0)
+    kk_img_right = pg.transform.flip(kk_img,True, False)
+
+    kk_img_sad = pg.image.load("ex02/fig/8.png")
     kk_rct = kk_img.get_rect()  #練習３こうかとんSurfaceのrect抽出
     kk_rct.center = 900, 400  #練習３：こうかとんの初期座標
     enn = pg.Surface((20,20))  #練習１　一片が２０の正方形を描画
@@ -35,17 +44,19 @@ def main():
     enn_rct = enn.get_rect()
     enn_rct.centerx = random.randint(0,WIDTH)
     enn_rct.centery = random.randint(0,HEIGHT)
+    accs = [a for a in range(1, 11)]
     vx, vy = +5, +5  #練習２　爆弾の移動
     clock = pg.time.Clock()
     tmr = 0
     while True:
-        
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         if kk_rct.colliderect(enn_rct):  #練習５こうかとんの衝突
+            screen.blit(kk_img_sad,0,1.0)
             print("Game Over")
             return
+            
         key_lst = pg.key.get_pressed() #キーが押されたら
         sum_mv = [0, 0]
         for k, tpl in delta.items():
@@ -56,9 +67,23 @@ def main():
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
         if check_bound(kk_rct) !=(True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        
         screen.blit(kk_img, kk_rct) #練習３　こうかとんの移動
-        enn_rct.move_ip(vx,vy)
+    
         yoko, tate = check_bound(enn_rct)
+        if key_lst[pg.K_UP]:
+           
+            screen.blit(kk_img_up,kk_rct)
+        if key_lst[pg.K_DOWN]:
+        
+           screen.blit(kk_img_down,kk_rct)
+        if key_lst[pg.K_LEFT]:
+             
+            screen.blit(kk_img_left,kk_rct)
+        if key_lst[pg.K_RIGHT]:
+            
+            screen.blit(kk_img_right,kk_rct)
+        avx, avy = vx*accs[min(tmr//500, 9)], vy*accs[min(tmr//500,9)]
         if not yoko:  #横にはみ出たら
             vx *= -1
         if not tate:  #縦にはみ出たら
