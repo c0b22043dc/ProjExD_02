e401e0e2d675ae07e4726b4b08395f1d0180754b
riku.k <c0b22043@edu.teu.ac.jp>
2023-11-28 14:21:55 +0900

    練習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -11,6 +12,12 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    enn = pg.Surface((20,20))  #練習１　一片が２０の正方形を描画
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10 )  #半径１０の赤い円の描画
+    enn.set_colorkey((0, 0, 0))  #黒の透明化
+    enn_rct = enn.get_rect()
+    enn_rct.centerx = random.randint(0,WIDTH)
+    enn_rct.centery = random.randint(0,HEIGHT)
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -20,6 +27,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(enn,enn_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
